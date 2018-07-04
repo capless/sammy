11d3a0581cbfb93825063899b930d5fb2249ece1
Brian Jinwright <brian@ipoots.com>
2018-07-04 00:00:24 -0400

    updated version to 0.3.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 
-version = '0.3.8'
+version = '0.3.9'
 
 setup(
     name='sammy',
